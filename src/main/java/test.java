6a4b7345f8a93face947eba737e9e79ac066c84c
ENGINEER  KOWERO <ENGINEER  KOWERO@DESKTOP-R6H7V2T>
2020-06-19 01:34:10 -0700

    a first git trial

---
+++ src/main/java/test.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class test {
+
+    public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    int a= in.nextInt();
+    int b= in.nextInt();
+    int c= a*b;
+    System.out.println(c);
+    
+    }
+}  
+    
+    
+
