7622e1e04c8b5ad2d26dda7ad3df99e9d8e3dff1
charity1475 <charitymbisi@gmail.com>
2020-06-24 18:31:29 +0300

    I have just change it to plus

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,7 +6,7 @@ public class test {
     Scanner in=new Scanner(System.in);
     int a= in.nextInt();
     int b= in.nextInt();
-    int c= a*b;
+    int c= a+b;
     System.out.println(c);
     
     }
